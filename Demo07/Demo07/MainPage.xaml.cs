860af029c3721a633f36f983dd2ab93e5ba0b3c6
Alumno <Alumno@LAB1509-02>
2023-05-19 15:19:06 -0500

    Agregar archivos de proyecto.

---
+++ Demo07/Demo07/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Demo07.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo07
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpeak_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<ITextToSpeech>().Speak(txtMessage.Text);
+        }
+    }
+}
